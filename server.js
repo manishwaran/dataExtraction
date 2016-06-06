daadb83ec25f4d024649af8797b174bf0778b8af
manishwaran <manishwaran.np@gmail.com>
2016-06-06 15:41:08 +0530

    Adding health check

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import healthCheck from 'connect-health-check'
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser'),
@@ -10,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 app.use(bodyParser.text())
+app.use(healthCheck)
 
 app.use(express.static(__dirname + '/'));
 app.get('/', function (req, res) {
